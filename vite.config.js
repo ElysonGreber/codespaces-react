7f08ecaa2e4c3b011b4e3de9f3efef8d48ad825c
BlackBloc <49597405+ElysonGreber@users.noreply.github.com>
2025-09-22 04:22:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['devserver-preview--tokepo.netlify.app'],}
+    allowedHosts: ['devserver-preview--tokepo.netlify.app'],},
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
